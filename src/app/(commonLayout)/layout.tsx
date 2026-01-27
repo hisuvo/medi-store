3f39d47a49fbf8113e0730aa7a3861b16455b40c
hisuvo <suvodatta72@gmail.com>
2026-01-27 11:00:32 +0600

    footer componente add done

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/layouts/footer";
 import { Navbar } from "@/components/layouts/navbar";
 
 export default function CommonLayout({
@@ -11,6 +12,9 @@ export default function CommonLayout({
         <Navbar />
       </div>
       {children}
+      <div>
+        <Footer />
+      </div>
     </section>
   );
 }
