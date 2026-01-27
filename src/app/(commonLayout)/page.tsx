f2cdb4699891515baa0148546d60027d99236cec
hisuvo <suvodatta72@gmail.com>
2026-01-27 09:25:19 +0600

    medi-store initial setup done with nextjs and shadcn ui

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-3xl">this is common layout</h2>
+    </div>
+  );
+}
---
+++ src/app/(commonLayout)/shop/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function ShopPage() {
+  return (
+    <div>
+      <Button variant="outline">Click me</Button>
+    </div>
+  );
+}
+
+export default ShopPage;
